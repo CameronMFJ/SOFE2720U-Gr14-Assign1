9ee1329f1e01b2d0fc7a91c7367671a8012d1491
CameronMFJ <47001696+CameronMFJ@users.noreply.github.com>
2020-02-12 23:22:03 -0500

    Update game.js

--- static/game.js
+++ static/game.js
@@ -23,16 +23,17 @@ document.addEventListener('keydown', function(event) {
   }
   choice.made = true;
   socket.emit('choice', choice);
-  
+  });
   socket.on('state', function(players) {
-  context.clearRect(0, 0, 800, 600);
-  context.fillStyle = 'green';
-  for (var id in players) {
-    var player = players[id];
-    context.beginPath();
-    context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
-    context.fill();
-  }
+	// display message of who won
+  	// update label for player 1 wins with player[1].Win
+	// update label for player 1 draws with players[1].draw
+	// update label for player 2 wins with player[2].Win
+	// update label for player 2 draws with players[2].draw
+	// delay for like 2 seconds
+	// display "please pick your weapon" message
+	
+  
 });
 
 
